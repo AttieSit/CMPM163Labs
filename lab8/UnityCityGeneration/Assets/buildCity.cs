71b3bc0a683c1ff7d61b28beb4dcbe248c9c2419
AttieSit <atsit@ucsc.edu>
2020-05-21 18:12:23 -0700

    added city buildings assets spaced out into scene with script

---
+++ lab8/UnityCityGeneration/Assets/buildCity.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildCity : MonoBehaviour
+{
+	public GameObject[] buildings;
+	public int mapWidth = 20;
+	public int mapHeight = 20;
+	int buildingFootprint = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    for(int h = 0; h < mapHeight; h++){
+    	for(int w = 0; w < mapWidth; w++){
+    		Vector3 pos = new Vector3(w * buildingFootprint,0,h * buildingFootprint);
+    		int n = Random.Range(0,buildings.Length); 
+    		Instantiate(buildings[n], pos, Quaternion.identity); 
+    		}
+    	}
+    }
+}
